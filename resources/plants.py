a8545389d31c60267f8007588d488e30c96e7b7c
David Arrizon <arrizon.david@gmail.com>
2019-11-13 17:11:03 -0600

    set up resources

---
+++ resources/plants.py
@@ -0,0 +1,16 @@
+import models
+
+from flask import Blueprint, jsonify, request
+
+from playhouse.shortcuts import model_to_dict
+
+plant = Blueprint('plants', 'plant')
+
+@plant.route('/' methods=["GET"])
+def get_all_plants():
+	try:
+		plants = [model_to_dict(plant) for plant in models.Plant.select()]
+		print(plants)
+		return jsonify(data=dogs, status={"code": 200, "message": "Success"})
+	except models.DoesNotExist:
+		return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
