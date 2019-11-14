7e02fdbc3c5a00db9df748baefdec597bcb75c6f
David Arrizon <arrizon.david@gmail.com>
2019-11-14 13:59:23 -0600

    created Post route and tested it in postman

--- resources/plants.py
+++ resources/plants.py
@@ -14,3 +14,18 @@ def get_all_plants():
 		return jsonify(data=plants, status={"code": 200, "message": "Success"})
 	except models.DoesNotExist:
 		return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
+
+# Create Route
+@plant.route('/', methods=["POST"])
+def create_plants():
+	payload = request.get_json()
+
+	plant = models.Plant.create(**payload)
+
+	print(plant.__dict__)
+
+	print(model_to_dict(plant), 'model to dict')
+
+	plant_dict = model_to_dict(plant)
+
+	return jsonify(data=plant_dict, status={"code": 201, "message": "Success"})
